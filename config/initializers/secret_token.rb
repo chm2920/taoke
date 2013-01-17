1ff8b2b64c56c019e8eedd0afd1cb7104cc4f54f
pathfinder <chm2920@gmail.com>
2013-01-17 23:13:02 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taobaoke::Application.config.secret_token = 'd80680357ff7855d6343bf3ca4321bf0700efedf2c4596e195eb3ca8daeb143db02c929a2d0cc674a955663a39cbe3b9f0e7e643eea2426685fe5a257d3ec7b6'
